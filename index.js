4eb11957db30eb917e809d9c87679fbd74647881
aneesRiazChaudhary <132878126+aneesRiazChaudhary@users.noreply.github.com>
2023-09-20 02:15:57 +0500

    “functionality”

--- index.js
+++ index.js
@@ -10,5 +10,34 @@ function myFunction() {
 }
 
 
+let btnn=document.getElementsByTagName('button');
+
+
+
+for (let i = 0; i < btnn.length; i++) {
+  btnn[i].style='background-color:blue;width:5rem;text-align:center';
+
+  btnn[i].onmouseover = function (e) { 
+    // console.log(e.target); 
+    e.target.style='background-color:green;padding:1rem;margin:1rem;'
+
+}
+
+btnn[i].onmouseout = function (e) { 
+  // console.log(e.target); 
+  e.target.style='background-color:blue;width:5rem;text-align:center';
+
+}
+
+
+  
+}
+
+
+
+
+
+
+
 
 
